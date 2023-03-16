bafc608d52b43852bcf7b9d44b1ed511287a7eee
Shaun Loughman <21343927@stu.mmu.ac.uk>
2023-03-16 23:23:16 +0000

    User Functionality (Not Finished Yet)

--- Backend/Controllers/users.controllers.js
+++ Backend/Controllers/users.controllers.js
@@ -1,5 +1,5 @@
 const users = require("users.models.js");
-
+const Joi = require("joi");
 
 const getAll =(req,res) => {
     users.getAllUsers((err, num_rows, results) => {
@@ -9,6 +9,27 @@ const getAll =(req,res) => {
     })
 }
 
+const addNew = (req, res) => {
+    const schema = Joi.object({
+        "user_name": Joi.string().required(),
+        "email": Joi.string().required().email({minDomainSegments: 2}),
+        "password": Joi.string().required().pattern(new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,25}$/)) //RegEx may not work (same one as i used for full stack which was tempramental)
+    })
+
+    const { error } = schema.validate(req.body); //Validates incoming request
+    if(error) return res.status(400).send(error.details[0].message);
+
+    let user = Object.assign({},req.body)
+
+    users.addNewUser(user, (err, id) => {
+        if(err) return res.sendStatus(500) //Fail
+
+        return res.status(201).send({user_id: id})
+    })
+}
+
+
 module.exports = {
-    getAll:getAll
+    getAll: getAll,
+    addNew: addNew,
 } 
--- Backend/models/users.models.js
+++ Backend/models/users.models.js
@@ -1,4 +1,6 @@
 const db = require("../database.js");
+const crypto = require("crypto")
+
 
 const getAllUsers = (done) => {
     const results =[];
@@ -11,6 +13,7 @@ const getAllUsers = (done) => {
 
             results.push({
                 user_id: row.user_id,
+                user_name: row.user_name,
                 email: row.email
             });
         },
@@ -20,6 +23,40 @@ const getAllUsers = (done) => {
     )
     }
 
+
+ const addNewUser = (user, done) => {    //Creates New User
+    const salt = crypto.randomBytes(64);     //Adds salt and hashes password
+    const hash = getHash(user.password, salt);
+    
+        const sql = "INSERT INTO users (user_name, email, password, salt) VALUES (?,?,?,?,?)"
+        let values = [user.user_name, user.email, hash, salt.toString("hex")];   //Adds values to query
+    
+        db.run(sql, values, function(err) {  //executes SQL query
+            if(err) return done(err)
+    
+            return done(null,this.lastID); //returns user id - success
+        })
+    }
+  
+  const getOne = (id, done) => {
+        const sql = "SELECT * FROM users WHERE user_id = ?"
+
+        db.get(sql,[id], (err,row) => {
+            if(err) return done(err)
+            if(!row) return done(404)
+
+
+            return done(null, {
+                user_id: row.user_id,
+                user_name: row.user_name,
+                email: row.email
+            })
+        })
+  }  
+
+
+
     module.exports = {
-        getAllUsers: getAllUsers
+        getAllUsers: getAllUsers,
+        addNewUser: addNewUser
     }
