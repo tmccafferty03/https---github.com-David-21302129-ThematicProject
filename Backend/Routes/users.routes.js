2e328f909a70d4b08805eca5016f06630dc57e37
Shaun Loughman <21343927@stu.mmu.ac.uk>
2023-03-16 23:20:55 +0000

    Added Some User Functionality

--- Backend/Routes/users.routes.js
+++ Backend/Routes/users.routes.js
@@ -1,10 +1,14 @@
+const users = require("../Controllers/users.controllers");
+
+
+
 module.exports = function(app){
     
     
     app.route("/user")
-        .get() //Get All users
+        .get(users.getAll) //Get All users
         .patch() //Update User (May be unneeded)
-        .post() //add new user
+        .post(users.addNew) //add new user
 
 
     app.route("/user/:user_id")
